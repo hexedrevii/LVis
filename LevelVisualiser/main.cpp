2ea30f557fca8d90e1360e94d5729db4bf69b739
HexedRevii <143884194+hexedrevii@users.noreply.github.com>
2025-04-02 12:09:55 +0300

    Add level impl.

--- LevelVisualiser/main.cpp
+++ LevelVisualiser/main.cpp
@@ -13,11 +13,22 @@ int main(int argc, char** argv)
 	double increase = 0.15;
 	CLI::Option* increase_opt = app.add_option("-i,--increase,/increase", increase, "Level Increase")->capture_default_str();
 
-	unsigned int levels = 10;
+	unsigned int levels = 0;
 	CLI::Option* levels_opt = app.add_option("-l,--levels,/levels", levels, "Amount of levels to show.")->capture_default_str();
 
 	unsigned int level = 0;
-	CLI::Option* level = app.add_option("-s,--level,/level", levels, "Show a specific level.")->capture_default_str();
+	CLI::Option* level_opt = app.add_option("-s,--level,/level", level, "Show a specific level.")->capture_default_str();
 
 	CLI11_PARSE(app, argc, argv);
+ 
+	if (level > 0)
+	{
+		int xp = (int)std::floor(std::pow((level / increase), gap));
+		std::cout << "level " << level << ": " << xp << " XP" << std::endl;
+
+		if (levels != 0)
+		{
+			std::cout << std::endl;
+		}
+	}
 }
