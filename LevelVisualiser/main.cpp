f8e7ba20ecb9ea6a86abdf27a57df012f087cfd1
HexedRevii <143884194+hexedrevii@users.noreply.github.com>
2025-04-02 12:03:50 +0300

    Add arguments.

--- LevelVisualiser/main.cpp
+++ LevelVisualiser/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <format>
 
-int main()
+#include <CLI/CLI.hpp>
+
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+	CLI::App app("Visualise level XP");
+
+	double gap = 2;
+	CLI::Option* gap_opt = app.add_option("-g,--gap,/gap", gap, "Level Gap")->capture_default_str();
+
+	double increase = 0.15;
+	CLI::Option* increase_opt = app.add_option("-i,--increase,/increase", increase, "Level Increase")->capture_default_str();
+
+	unsigned int levels = 10;
+	CLI::Option* levels_opt = app.add_option("-l,--levels,/levels", levels, "Amount of levels to show.")->capture_default_str();
+
+	unsigned int level = 0;
+	CLI::Option* level = app.add_option("-s,--level,/level", levels, "Show a specific level.")->capture_default_str();
+
+	CLI11_PARSE(app, argc, argv);
 }
